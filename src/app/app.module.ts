312353e8304337dc6ab913ddc55a780a048efcc3
Guillerbr <ghminvest@gmail.com>
2019-06-26 00:25:27 -0300

    commit 003-Add forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+
+import { EmployeeService } from './shared/employee.service';
 import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -8,6 +10,8 @@ import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
 import { EmployeeComponent } from './employees/employee/employee.component';
 import { EmployeeListComponent } from './employees/employee-list/employee-list.component';
+import { FormsModule } from "@angular/forms";
+
 
 @NgModule({
   declarations: [
@@ -19,9 +23,10 @@ import { EmployeeListComponent } from './employees/employee-list/employee-list.c
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/employee.service.ts
+++ src/app/shared/employee.service.ts
@@ -1,9 +1,12 @@
+import { Employee } from './employee.model';
 import { Injectable } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+    formData :Employee;
 
   constructor() { }
 }
